939ea92c20a0a13813eee46902484dd626eabc08
MateoC4BR4L <juan.cabral@correo.ucu.edu.uy>
2023-07-11 08:28:53 -0300

    first

---
+++ TodoApi/Animal.cs
@@ -0,0 +1,12 @@
+﻿namespace TodoApi;
+
+public class Animal
+{
+    public string id { get; set; }
+
+    public string especie { get; set; }
+    
+    public string nombre { get; set; }
+    
+    public bool vacunado { get; set; }
+}
---
+++ TodoApi/Controllers/AnimalController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TodoApi.Controllers;
+
+[ApiController]
+[Route("[animales]")]
+public class AnimalController : ControllerBase
+{
+    [HttpGet]
+    [Route("listar")]
+    public dynamic listar()
+    {
+        
+    }
+    
+    [HttpPost]
+    [Route("guardar")]
+    public dynamic guardar()
+    {
+        
+    }
+}
